20d99558e259979ecb239062205105d7153259d0
Li Wang <lilywang888@gmail.com>
2018-01-17 12:52:30 -0800

    Li Wang A00994581

--- Hello World/Hello World/lab1.cpp
+++ Hello World/Hello World/lab1.cpp
@@ -1,6 +1,9 @@
 #include "lab1.h"
 
 
+
+
+
 // Greatest Common Denominator
 // PRE: a is a positive integer
 // PRE: b is a positive integer
@@ -8,16 +11,36 @@
 // RETURN: the greatest common denominator of a and b.
 int gcd(const int a, const int b)
 {
-	// Replace this line with your code (have fun!).
-	return 0;
+	int bignum = { 1 };
+	int i = { 1 };
+	if (a < b) {
+		bignum = { a };
+	}
+	else {
+		bignum = { b };
+	}
+
+	for (int i = bignum; i > 0; i--) {
+		if ((a%i) ==0 && (b % i) == 0) {
+			return i;
+		}
+		
+	}
+	
 }
 
 // Fibonacci
 // PRE: n is a positive int greater than 0
 // POST: n is not changed
 // RETURN: the nth positive integer in the Fibonacci sequence.
-long long fibonacci(const int n)
-{
-	// Replace this line with your code (challenge: do this with recursion!).
-	return 0;
-}
+	long long fibonacci(const int n)
+	{
+		if (n == 1 || n == 2) {
+			return 1;
+		}
+		else {
+			return(fibonacci(n - 1) + fibonacci(n - 2));
+
+		}
+	}
+
